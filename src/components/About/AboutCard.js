475155ef3965baa42263a4a2fdd585aa5aa655ed
Neddy AP <neddy1298@gmail.com>
2024-07-06 21:16:56 +0700

    init

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Neddy Avgha Prasetio </span>
+            from <span className="purple"> Cicurug, Indonesia.</span>
+            <br />
+            I am currently studying for a Bachelor's degree in Computer Science.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading Comics
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Simple but better!"{" "}
+          </p>
+          <footer className="blockquote-footer">Neddy</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
